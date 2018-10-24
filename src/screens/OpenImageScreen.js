071ff9c84f5ab2ca0b6e18d4f987e8101640964a
SexWexPex <inetgameslam@gmail.com>
2018-10-25 00:58:37 +0300

    added image loading indicator

--- src/screens/OpenImageScreen.js
+++ src/screens/OpenImageScreen.js
@@ -1,20 +1,29 @@
 import React, {Component} from 'react';
-import {View, Image} from 'react-native';
+import {View, Image, ActivityIndicator} from 'react-native';
 import {imageFiller} from "../library/utils/imageResizer";
+
+
+const AppLoader = () => (
+    <View style={{alignItems: 'center'}}>
+        <ActivityIndicator size={"large"} color={'red'} style={{position: 'absolute', zIndex: 1}}/>
+    </View>
+);
 
 class OpenImageScreen extends Component {
 
     state = {
-        item: this.props.navigation.getParam('item', null)
+        item: this.props.navigation.getParam('item', null),
+        imageLoading: true
     };
 
     render() {
-        console.log(this.state.item);
         return (
             <View>
-                <Image source={{uri: this.state.item.urls.regular}} style={imageFiller(this.state.item)}/>
+                {this.state.imageLoading && <AppLoader/>}
+                <Image source={{uri: this.state.item.urls.regular}} style={imageFiller(this.state.item)}
+                       onLoad={() => this.setState({imageLoading: false})}/>
             </View>
-        );
+        )
     }
 }
 
